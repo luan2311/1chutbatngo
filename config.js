eab0bf286de790d9ff55847c359578022182d0c0
luan2311 <95701411+luan2311@users.noreply.github.com>
2022-06-01 09:21:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Hana <3';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy 1/6';
-const titleCard = 'Tặng người cute';
+const titleCard = 'Tặng ai đó';
 const contentCard = 'Chúc Hân có 1 ngày 1/6 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Đặc biệt là mau khỏi bệnh để tui đèo đi chơi nhen <3';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
