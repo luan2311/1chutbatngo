dfdf4f44dd5e9a508e3cd583c7c1d73eec3f9e18
luan2311 <95701411+luan2311@users.noreply.github.com>
2024-02-14 23:23:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'em bé nhỏ<3';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy Valentines';
-const titleCard = 'Tặng emmm';
+const titleCard = 'Tặng emmm nè';
 const contentCard = 'Chúc bé ngày càng xink đẹp, iu thương bản thân hơn. Chúc em lun bình an mạnh khỏe zui zẻ và lun lun hạnh phúc. Dù có ua bao thăng trầm thì mong ta lun có nhau nhé<33';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
